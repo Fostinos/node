2667e7bada3cb4921ab9f07663a276e150f2f5a6
Fostinos <kafostin@gmail.com>
2023-12-14 15:19:46 +0100

    debug messages

--- LoRaMAC/LoRaMAC.py
+++ LoRaMAC/LoRaMAC.py
@@ -250,6 +250,7 @@ class LoRaMAC():
         self._LoRa.setFrequency(self._region.uplink_frequency(self._channel))
         self._LoRa.setLoRaModulation(self._spreading_factor, self._region.value.UPLINK_BANDWIDTH, LORA_CODING_RATE)
         self._LoRa.setLoRaPacket(self._LoRa.HEADER_EXPLICIT, LORA_PREAMBLE_SIZE, LORA_PAYLOAD_MAX_SIZE, UPLINK_CRC_TYPE, UPLINK_IQ_POLARITY)
+        self._logger.debug(f"TX : FREQ = {self._region.uplink_frequency(self._channel)} Hz, SF = {self._spreading_factor}")
     
     def __radio_rx1_mode(self):
         self._LoRa.purge(LORA_PAYLOAD_MAX_SIZE)
@@ -258,6 +259,7 @@ class LoRaMAC():
         self._LoRa.setLoRaModulation(self._spreading_factor, self._region.value.DOWNLINK_BANDWIDTH, LORA_CODING_RATE)
         self._LoRa.setLoRaPacket(self._LoRa.HEADER_EXPLICIT, LORA_PREAMBLE_SIZE, LORA_PAYLOAD_MAX_SIZE, DOWNLINK_CRC_TYPE, DOWNLINK_IQ_POLARITY)
         self._LoRa.request(self._LoRa.RX_CONTINUOUS)
+        self._logger.debug(f"RX1 : FREQ = {self._region.downlink_frequency(self._channel)} Hz, SF={self._spreading_factor}")
         
     def __radio_rx2_mode(self)-> bool:
         self._LoRa.purge(LORA_PAYLOAD_MAX_SIZE)
@@ -266,6 +268,7 @@ class LoRaMAC():
         self._LoRa.setLoRaModulation(self._region.value.SPREADING_FACTOR_MAX, self._region.value.DOWNLINK_BANDWIDTH, LORA_CODING_RATE)
         self._LoRa.setLoRaPacket(self._LoRa.HEADER_EXPLICIT, LORA_PREAMBLE_SIZE, LORA_PAYLOAD_MAX_SIZE, DOWNLINK_CRC_TYPE, DOWNLINK_IQ_POLARITY)
         self._LoRa.request(self._LoRa.RX_CONTINUOUS)
+        self._logger.debug(f"RX2 : FREQ = {self._region.value.RX2_FREQUENCY} Hz, SF = {self._spreading_factor}")
 
     def __radio_transmit(self, delay:int)-> bool:
         self._LoRa.beginPacket()
