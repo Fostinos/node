b632587229fa29277a4a30178a8291667628a0cb
Fostinos <kafostin@gmail.com>
2023-12-15 17:46:15 +0100

    Test

--- App/App.py
+++ App/App.py
@@ -46,7 +46,7 @@ class App():
         while True:
             if self.__LoRaWAN.is_joined():
                 self.__logger.info("The device transmits data")
-                self.__LoRaWAN.transmit(bytes([0x01, 0x02, 0x03, 0x04, 0x05, 0x6, 0x07, 0x08, 0x09]), True)
+                self.__LoRaWAN.transmit(bytes([0x01, 0x02, 0x03, 0x04, 0x05, 0x6, 0x07, 0x08, 0x09]), False)
             time.sleep(30)
 
 
