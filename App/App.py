218d6a4cdc9fd5f584810b6d1b4ea2c3ff2d0613
Fostinos <kafostin@gmail.com>
2023-12-15 16:06:21 +0100

    Improvements

--- App/App.py
+++ App/App.py
@@ -45,7 +45,7 @@ class App():
         self.__LoRaWAN.join(max_tries=3, forced=True)
         while True:
             if self.__LoRaWAN.is_joined():
-                self.__logger.info("The device transmit data")
+                self.__logger.info("The device transmits data")
                 self.__LoRaWAN.transmit(bytes([0x01, 0x02, 0x03, 0x04, 0x05, 0x6, 0x07, 0x08, 0x09]), True)
             time.sleep(30)
 
--- LoRaMAC/LoRaMAC.py
+++ LoRaMAC/LoRaMAC.py
@@ -480,7 +480,7 @@ class LoRaMAC():
             DevAddr = bytearray(self._device.downlinkPhyPayload[1:5])
             DevAddr.reverse()
             if int(DevAddr.hex(), 16) != int(self._device.DevAddr.hex(), 16):
-                self._logger.debug(f"LoRaWAN : Downlink DevAddr Mismatching")
+                self._logger.debug(f"LoRaWAN : Downlink DevAddr Mismatching {DevAddr.hex()} != {self._device.DevAddr.hex()}")
                 return False
             
             response = WrapperLoRaMAC.data_down(self._device.downlinkPhyPayload, self._device.DevAddr,
