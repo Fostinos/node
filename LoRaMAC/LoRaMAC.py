6d23679f908c43780be3b6360b8c0e285d6ecc63
Fostinos <kafostin@gmail.com>
2023-12-15 21:06:26 +0100

    Logging messages

--- LoRaMAC/LoRaMAC.py
+++ LoRaMAC/LoRaMAC.py
@@ -408,7 +408,7 @@ class LoRaMAC():
             return bytes([])
         status = self._LoRa.status()
         if status != self._LoRa.STATUS_RX_DONE and status != self._LoRa.STATUS_TX_DONE:
-            self._logger.error(f"LoRa:{RadioStatus(status)}")
+            self._logger.error(f"RX  : LoRa {RadioStatus(status)}")
             return bytes([])
         return self._LoRa.get(self._LoRa.available())
         
