c2d53eafe284befdbf1f70f24d1018cb4863cd41
Fostinos <kafostin@gmail.com>
2024-01-06 12:20:06 +0100

    Get MAC commands

--- LoRaMAC/loramac_wrapper.py
+++ LoRaMAC/loramac_wrapper.py
@@ -183,7 +183,7 @@ class WrapperLoRaMAC :
         output["ADR"]=bool(mac.FHDR.FCtrl.downlink.ADR)
         output["RFU"]=bool(mac.FHDR.FCtrl.downlink.RFU)
         output["ACK"]=bool(mac.FHDR.FCtrl.downlink.ACK)
-        output["FOptsLen"]=bool(mac.FHDR.FCtrl.downlink.FOptsLen)
+        output["FOptsLen"]=int(mac.FHDR.FCtrl.downlink.FOptsLen)
         output["FPending"]=bool(mac.FHDR.FCtrl.downlink.FPending)
         output["FCntDown"]=int(mac.FHDR.FCnt16)
         output["FPortDown"]=int(mac.FPort)
