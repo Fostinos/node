4891470e7b16f57ee3998d9c416ba49035d388b3
Fostinos <kafostin@gmail.com>
2025-07-29 11:10:28 +0000

    Improvements

--- LoRaMAC/LoRaMAC.py
+++ LoRaMAC/LoRaMAC.py
@@ -438,7 +438,8 @@ class LoRaMAC():
         self._LoRa.endPacket()
         self._logger.debug(f"UP  : PHYPAYLOAD = {self._device.uplinkPhyPayload.hex()}")
         time.sleep(delay - 0.4)
-        return self._LoRa.wait(0.1)
+        self._LoRa.wait(0.1)
+        return True
 
     def __radio_receive(self, delay:int)-> bytes:
         self._LoRa.wait()
